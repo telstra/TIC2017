fd90af00a093dcf4e290f626c89d2b5c86bbccc8
Matt Sinclair <matthew.sinclair@team.telstra.com>
2017-03-24 21:07:54 +1100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TIC2017
 Telstra Innovation Challenge 2017
+
+This repository will contain all the information required to succeed in the Telstra Innovation Challenge.
+
+Make sure to check out the [Wiki page](https://github.com/telstra/TIC2017/wiki) to access the Getting Started guide!
+
+We've also uploaded some example code for you to access, as well as documentation so you can understand all about the Telstra Cat-1 Shield.
---
+++ RegisterLightMeasurementDevice/RegisterLightMeasurementDevice.ino
@@ -0,0 +1,70 @@
+#include <connection4g.h>
+#include <telstraiot.h>
+#include <iotshield.h>
+#include <shieldinterface.h>
+
+ShieldInterface shieldif;
+IoTShield shield(&shieldif);
+Connection4G conn(true,&shieldif);
+
+const char host[] = "tic2017team000.iot.telstra.com";
+const char tenant[] = "tic2017team000";
+const char username[] = "device";
+const char password[] = "XXXXXX";
+const char appkey[] = ""; 
+
+TelstraIoT iotPlatform(host, tenant, username, password, appkey, &conn, &shield);
+
+void setup() {
+  Serial.begin(115200);
+  while(Serial.available()==0); // Wait for serial character before starting
+  Serial.print("******* Waiting for shield *********");
+  shield.waitUntilShieldIsReady();
+
+
+
+  // Check if shield is connected to 4G network
+  if(shield.isPDPContextActive()) {
+      
+    	  // Open secure TCP connection to IoT Platform host
+        Serial.println("############################ OPENING TCP CONNECTION #########################");
+        conn.openTCP(host,443);  
+    
+     	  // Register device on IoT Platform
+        Serial.println("############################ REGISTERING DEVICE #############################");
+        char id[8];
+        const char* supportedMeasurements[3];
+        supportedMeasurements[0] = "XAcc";
+        supportedMeasurements[1] = "YAcc";
+        supportedMeasurements[2] = "ZAcc";
+    
+        int result = iotPlatform.registerDevice("Acc. Device", id, 8, supportedMeasurements, 1);
+    
+        if(result<0) {
+          Serial.println(F("Registration error."));
+          while(true);
+        } else {
+    	    Serial.print(F("Arduino registered with id: "));
+          Serial.println(id);
+    	  }
+      
+      	// Close the TCP connection to the server
+        Serial.println("############################ REGISTRATION COMPLETE #########################");
+        Serial.println("Please wait while connection closes...");
+      	conn.closeTCP();
+        Serial.println("Done!");
+        delay(1000);
+
+
+        
+	
+  } else {
+        Serial.println(F("Shield is not connected to 4G network!"));
+        while(true);
+  }
+}
+
+void loop() {
+
+}
+
